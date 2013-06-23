4afbc0af253046952fa589fa1a7f2b44fb4a313b
bridget <bkey23@gmail.com>
2013-06-23 16:58:12 -0400

    init

---
+++ get_citibike.php
@@ -0,0 +1,19 @@
+<?php
+
+$curl = curl_init();
+curl_setopt_array($curl, Array(
+	CURLOPT_URL            => "http://citibikenyc.com/stations/json",
+	CURLOPT_TIMEOUT        => 300,
+	CURLOPT_CONNECTTIMEOUT => 60,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_SSL_VERIFYHOST => false,
+	CURLOPT_SSL_VERIFYPEER => false,
+	CURLOPT_ENCODING       => 'gzip,deflate'
+));
+$json = curl_exec($curl);
+
+print_r($json);
+
+
+
+?>
